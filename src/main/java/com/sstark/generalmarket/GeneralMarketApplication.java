aabb72c2ad2700c380a4a8996103cc952cac844c
Sstark97 <aitor@leanmind.es>
2023-07-20 20:58:21 +0100

    Project started

---
+++ src/main/java/com/sstark/generalmarket/GeneralMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sstark.generalmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sstark/generalmarket/GeneralMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sstark.generalmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
