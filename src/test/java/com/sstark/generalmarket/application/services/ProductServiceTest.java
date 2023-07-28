97c5c88af2493500be14b0b897b4250de37cf71b
Sstark97 <aitor@leanmind.es>
2023-07-28 17:04:17 +0100

    Test(:recycle:) ProductServiceTest find all products by first category (Add BeforeAll method and some new variables)

--- src/test/java/com/sstark/generalmarket/application/services/ProductServiceTest.java
+++ src/test/java/com/sstark/generalmarket/application/services/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.sstark.generalmarket.application.services;
 
 import com.sstark.generalmarket.domain.models.Product;
 import com.sstark.generalmarket.domain.repositories.ProductRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -11,30 +12,30 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductServiceTest {
+    private static ProductRepository productRepository;
+    private static ProductService productService;
+
+    @BeforeAll
+    public static void setup() {
+        productRepository = Mockito.mock(ProductRepository.class);
+        productService = new ProductService(productRepository);
+    }
+
     @Test
     public void find_all_products() {
-        ProductRepository productRepository = Mockito.mock(ProductRepository.class);
-        ProductService productService = new ProductService(productRepository);
-
-        Mockito.when(productRepository.findAll()).thenReturn(Arrays.asList(
-                new Product(1,"Product 1", 1,"12345", 10.0, 3, true),
-                new Product(2,"Product 2", 1,"2341", 20.0, 4 , false)
-        ));
+        List<Product> allProducts = Arrays.asList(
+                new Product(1, "Product 1", 1, "12345", 10.0, 3, true),
+                new Product(2, "Product 2", 1, "2341", 20.0, 4, false)
+        );
+        Mockito.when(productRepository.findAll()).thenReturn(allProducts);
 
         List<Product> products = productService.findAll().orElseThrow();
 
-        assertEquals(
-                Arrays.asList(
-                        new Product(1,"Product 1", 1,"12345", 10.0, 3, true),
-                        new Product(2,"Product 2", 1,"2341", 20.0, 4 , false)
-                ), products
-        );
+        assertEquals(allProducts, products);
     }
 
     @Test
     public void find_all_products_by_first_category() {
-        ProductRepository productRepository = Mockito.mock(ProductRepository.class);
-        ProductService productService = new ProductService(productRepository);
         List<Product> allProducts = Arrays.asList(
                 new Product(1,"Product 1", 1,"12345", 10.0, 3, true),
                 new Product(2,"Product 2", 2,"2341", 20.0, 4 , false)
