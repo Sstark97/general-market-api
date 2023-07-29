cc828ac6be4abfdfb61e5d6eed5ed529faa5f82f
Sstark97 <aitor@leanmind.es>
2023-07-29 11:22:23 +0100

    Test(:white_check_mark:): PurchaseServiceTest get all purchases

--- src/main/java/com/sstark/generalmarket/application/services/PurchaseService.java
+++ src/main/java/com/sstark/generalmarket/application/services/PurchaseService.java
@@ -1,11 +1,18 @@
 package com.sstark.generalmarket.application.services;
 
+import com.sstark.generalmarket.domain.models.Purchase;
 import com.sstark.generalmarket.domain.repositories.PurchaseRepository;
+
+import java.util.List;
 
 public class PurchaseService {
     private PurchaseRepository purchaseRepository;
 
     public PurchaseService(PurchaseRepository purchaseRepository) {
         this.purchaseRepository = purchaseRepository;
+    }
+
+    public List<Purchase> getAll() {
+        return purchaseRepository.getAll();
     }
 }
