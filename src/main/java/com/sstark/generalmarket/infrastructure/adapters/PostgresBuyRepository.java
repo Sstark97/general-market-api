0146008be26d37c227c7da4138ceb83efc17b22f
Sstark97 <aitor@leanmind.es>
2023-07-29 11:27:09 +0100

    :sparkles: Add purchase getAll method to adapter

--- src/main/java/com/sstark/generalmarket/infrastructure/adapters/PostgresBuyRepository.java
+++ src/main/java/com/sstark/generalmarket/infrastructure/adapters/PostgresBuyRepository.java
@@ -2,6 +2,7 @@ package com.sstark.generalmarket.infrastructure.adapters;
 
 import com.sstark.generalmarket.domain.models.Purchase;
 import com.sstark.generalmarket.domain.repositories.PurchaseRepository;
+import com.sstark.generalmarket.infrastructure.mappers.PurchaseMapper;
 import com.sstark.generalmarket.infrastructure.repositories.PurchaseJPARepository;
 import org.springframework.stereotype.Component;
 
@@ -10,13 +11,15 @@ import java.util.List;
 @Component
 public class PostgresBuyRepository implements PurchaseRepository {
     private final PurchaseJPARepository repository;
+    private final PurchaseMapper mapper;
 
-    public PostgresBuyRepository(PurchaseJPARepository repository) {
+    public PostgresBuyRepository(PurchaseJPARepository repository, PurchaseMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     @Override
     public List<Purchase> getAll() {
-        return null;
+        return mapper.toPurchases(repository.findAll());
     }
 }
